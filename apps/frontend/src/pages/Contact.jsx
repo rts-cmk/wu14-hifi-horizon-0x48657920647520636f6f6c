28b97e3965f961960d7f22fe624cb89105238551
Razzer101 <Rasmus@anders-net.dk>
2025-12-15 13:30:41 +0100

    Feat: contact page complete (no functions)

--- apps/frontend/src/pages/Contact.jsx
+++ apps/frontend/src/pages/Contact.jsx
@@ -1,17 +1,17 @@
 export default function Contact(){
     return(
         <>
-            <h1 className="main__title">Get in touch with us</h1>
+            <h1 className="main__title">GET IN TOUCH WITH US</h1>
             <section className="contact">
-                <form className="contact-form">
-                    <label className="contact-form__title">Full name <span className="contact-form__title--highlight">*</span></label>
-                    <input required className="contact-form__input" type="text" />
-                    <label className="contact-form__title">Email <span className="contact-form__title--highlight">*</span></label>
-                    <input required className="contact-form__input" type="email" />
-                    <label className="contact-form__title">Subject <span className="contact-form__title--highlight">*</span></label>
-                    <input required className="contact-form__input" type="text" />
-                    <label className="contact-form__title">Message <span className="contact-form__title--highlight">*</span></label>
-                    <textarea required className="contact-form__textarea"></textarea>
+                <form id="contact" className="contact-form">
+                    <label htmlFor="name" className="contact-form__title">Full name <span className="contact-form__title--highlight">*</span></label>
+                    <input id="name" required className="contact-form__input" type="text" />
+                    <label htmlFor="email" className="contact-form__title">Email <span className="contact-form__title--highlight">*</span></label>
+                    <input id="email" required className="contact-form__input" type="email" />
+                    <label htmlFor="subject" className="contact-form__title">Subject <span className="contact-form__title--highlight">*</span></label>
+                    <input id="subject" required className="contact-form__input" type="text" />
+                    <label htmlFor="message" className="contact-form__title">Message <span className="contact-form__title--highlight">*</span></label>
+                    <textarea id="message" required className="contact-form__textarea"></textarea>
                     <button className="contact-form__submit-btn" type="submit">Submit</button>
                 </form>
                 <p className="contact__extra-info">Visit our sister companies <span className="contact__extra-info--highlight">Home Sound</span> and <span className="contact__extra-info--highlight">The Movie Rooms</span> part of the HiFi Horizon Group.</p>
